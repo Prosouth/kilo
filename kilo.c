c751da72baf76ae3345ceaf82124b7b308c8b292
Sebastien Saez <monsieursaez@gmail.com>
2017-06-30 14:28:22 +0200

    ctrl-q to quit

--- kilo.c
+++ kilo.c
@@ -6,6 +6,9 @@
 #include <unistd.h>
 #include <termios.h>
 
+/*** defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /*** data ***/
 struct termios orig_termios;
 
@@ -62,7 +65,7 @@ int main()
         {
             printf("%d ('%c')\r\n", c, c);
         }
-        if(c == 'q') 
+        if(c == CTRL_KEY('q')) 
         {
             break;
         }
