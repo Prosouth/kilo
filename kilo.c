139921631b373a67b3cfbaa9dcfb0807ed54ad6c
Sebastien Saez <monsieursaez@gmail.com>
2017-06-30 15:16:13 +0200

    windows size, the hard way

--- kilo.c
+++ kilo.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 #include <termios.h>
 
@@ -12,6 +13,8 @@
 /*** data ***/
 struct editorConfig
 {
+    int screenrows;
+    int screencols;
     struct termios orig_termios;
 };
 
@@ -67,11 +70,27 @@ char editorReadKey()
     return c;
 }
 
+int getWindowSize(int* rows, int* cols)
+{
+    struct winsize ws;
+
+    if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) 
+    {
+        return -1;
+    }
+    else
+    {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 /*** output ***/
 void editorDrawRows()
 {
     int y;
-    for (y = 0; y < 24; y++)
+    for (y = 0; y < E.screenrows; y++)
     {
         write(STDOUT_FILENO, "~\r\n",3);
     }
@@ -102,9 +121,18 @@ void editorProcessKeypress()
 }
 
 /*** init ***/
+void initEditor()
+{
+    if(getWindowSize(&E.screenrows, &E.screencols) == -1)
+    {
+        die("getWindowSize");
+    }
+}
+
 int main()
 {
     enableRawMode();
+    initEditor();
 
     while (1)
     {
