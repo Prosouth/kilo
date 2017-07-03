7f954fe95b602dab5030db13183b4af93346a1f7
Sebastien Saez <monsieursaez@gmail.com>
2017-07-03 13:50:43 +0200

    oupsie

--- kilo.c
+++ kilo.c
@@ -97,7 +97,7 @@ int getWindowSize(int* rows, int* cols)
 {
     struct winsize ws;
 
-    if(1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) 
+    if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) 
     {
         if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
         {
